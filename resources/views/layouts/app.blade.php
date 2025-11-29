adff3a5271f2a2c473a406a3aa0c6790c92d536d
ForWarZz <forwarzz.dev@gmail.com>
2025-11-29 18:55:31 +0100

    feat: add tarteaucitron and default config

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config("app.name", "Laravel") }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link rel="preconnect" href="https://fonts.bunny.net" />
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
@@ -18,18 +18,48 @@
         <div class="min-h-screen">
             <x-nav-bar />
 
-{{--            <!-- Page Heading -->--}}
-{{--            @if (isset($header))--}}
-{{--                <header class="bg-white dark:bg-gray-800 shadow">--}}
-{{--                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">--}}
-{{--                        {{ $header }}--}}
-{{--                    </div>--}}
-{{--                </header>--}}
-{{--            @endif--}}
+            {{-- <!-- Page Heading --> --}}
+            {{-- @if (isset($header)) --}}
+            {{-- <header class="bg-white dark:bg-gray-800 shadow"> --}}
+            {{-- <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8"> --}}
+            {{-- {{ $header }} --}}
+            {{-- </div> --}}
+            {{-- </header> --}}
+            {{-- @endif --}}
 
             <main>
                 {{ $slot }}
             </main>
         </div>
     </body>
+
+    {{-- <script src="{{ asset("tarteaucitron/tarteaucitron.min.js") }}"></script> --}}
+    <script type="text/javascript">
+        tarteaucitron.init({
+            privacyUrl: '',
+            bodyPosition: 'bottom',
+            hashtag: '#tarteaucitron',
+            cookieName: 'tarteaucitron',
+            orientation: 'middle',
+            groupServices: false,
+            showIcon: true,
+            iconPosition: 'BottomRight',
+            adblocker: false,
+            DenyAllCta: true,
+            AcceptAllCta: true,
+            highPrivacy: true,
+            handleBrowserDNTRequest: false,
+            removeCredit: false,
+            moreInfoLink: true,
+            useExternalCss: false,
+            useExternalJs: false,
+            readmoreLink: '',
+        });
+
+        tarteaucitron.user.matomoId = 1;
+        tarteaucitron.user.matomoHost = '//ton-analytics.com/';
+        (tarteaucitron.job = tarteaucitron.job || []).push('matomo');
+
+        (tarteaucitron.job = tarteaucitron.job || []).push('googlemaps');
+    </script>
 </html>
