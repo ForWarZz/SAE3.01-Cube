9ec5b4db0a8c8118fe7d1f55dc0d9c479e2c70ca
ForWarZz <forwarzz.dev@gmail.com>
2025-12-10 18:57:11 +0100

    feat: add commercial seeder for initial data population

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Commercial;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Commercial::create([
+            'nom_commercial' => 'Jean',
+            'prenom_commercial' => 'Marc',
+            'email_commercial' => 'jean.marc@cube.fr',
+            'hash_mdp_commercial' => bcrypt('password123'),
+        ]);
     }
 }
