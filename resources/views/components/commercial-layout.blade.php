e3228fa87edf17d075ab9e7cac2f5c0c1d74f2ea
ForWarZz <forwarzz.dev@gmail.com>
2025-12-12 14:50:38 +0100

    feat: replace SVG icon with Heroicon for link in commercial layout

--- resources/views/components/commercial-layout.blade.php
+++ resources/views/components/commercial-layout.blade.php
@@ -35,14 +35,7 @@
                         href="{{ route("commercial.models.index") }}"
                         class="flex items-center rounded-md px-4 py-2 text-gray-300 hover:bg-slate-700 hover:text-white"
                     >
-                        <svg class="mr-3 h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z"
-                            ></path>
-                        </svg>
+                        <x-heroicon-o-cube-transparent class="mr-3 h-6 w-6" />
                         Modèles
                     </a>
 
@@ -50,14 +43,7 @@
                         href="{{ route("commercial.bikes.index") }}"
                         class="flex items-center rounded-md px-4 py-2 text-gray-300 hover:bg-slate-700 hover:text-white"
                     >
-                        <svg class="mr-3 h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                            ></path>
-                        </svg>
+                        <x-heroicon-o-tag class="mr-3 h-6 w-6" />
                         Vélos
                     </a>
 
