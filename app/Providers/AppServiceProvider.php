9936f6df6bcab48f676ca589e6a32fa38d9c246a
ForWarZz <forwarzz.dev@gmail.com>
2026-01-02 18:49:28 +0100

    feat: register BotMan alias in AppServiceProvider and update handle method in BotManController

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Client;
+use BotMan\BotMan\BotMan;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Cashier\Cashier;
@@ -12,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void {}
+    public function register(): void
+    {
+        $this->app->alias('botman', BotMan::class);
+    }
 
     /**
      * Bootstrap any application services.
