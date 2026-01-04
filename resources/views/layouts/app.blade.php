b95fa761cc2851556b899e1465fa95b5f1a3dd80
ForWarZz <forwarzz.dev@gmail.com>
2026-01-04 21:27:55 +0100

    fix: comment out Matomo configuration in app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -142,10 +142,10 @@
                 readmoreLink: '',
             });
 
-            tarteaucitron.user.matomoId = {{ config("services.matomo.site_id") }};
-            tarteaucitron.user.matomoHost = '{{ config("services.matomo.url") }}';
+            {{-- tarteaucitron.user.matomoId = {{ config("services.matomo.site_id") }}; --}}
+            {{-- tarteaucitron.user.matomoHost = '{{ config("services.matomo.url") }}'; --}}
 
-            (tarteaucitron.job = tarteaucitron.job || []).push('matomo');
+            // (tarteaucitron.job = tarteaucitron.job || []).push('matomo');
             (tarteaucitron.job = tarteaucitron.job || []).push('googleplaces');
         </script>
     </body>
