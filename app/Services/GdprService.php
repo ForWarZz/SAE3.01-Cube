d9c19a46ba70345a6b48dfb3f2cd10d2359fa459
ForWarZz <forwarzz.dev@gmail.com>
2026-01-09 08:50:02 +0100

    fix: handle potential null values for billing and delivery addresses in GdprService

--- app/Services/GdprService.php
+++ app/Services/GdprService.php
@@ -350,8 +350,8 @@ class GdprService
                     'pourcentage_remise' => $order->pourcentage_remise,
                     'moyen_paiement' => $order->paymentType->label_type_paiement,
                     'last4_cb' => $order->cb_last4,
-                    'adresse_facturation' => $order->billingAddress->toArray(),
-                    'adresse_livraison' => $order->deliveryAddress->toArray(),
+                    'adresse_facturation' => $order->billingAddress?->toArray() ?? [],
+                    'adresse_livraison' => $order->deliveryAddress?->toArray() ?? [],
                     'mode_livraison' => $order->shippingMode->label_moyen_livraison,
                     'articles' => $order->items->map(function (OrderLine $item) {
                         $article = $item->reference->article;
